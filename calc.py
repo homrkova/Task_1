28b794ba9fa13f9a942abef618593497f6c07e56
homrkova <79359305+homrkova@users.noreply.github.com>
2021-03-25 17:47:06 +0500

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,7 +1,7 @@
 while True:
     num = input()
     if num.isdigit():
-        num = float(num)
+        num = int(num)
         break
     else:
         print('Была введена строка, а не число.\nВведите число.')
@@ -9,17 +9,17 @@ flag = True
 while flag:
     OperatNum = input().split()
     if OperatNum[0] == '+':
-        num += float(OperatNum[1])
+        num += int(OperatNum[1])
     elif OperatNum[0] == '*':
-        num *= float(OperatNum[1])
+        num *= int(OperatNum[1])
     elif OperatNum[0] == '-':
-        num -= float(OperatNum[1])
-    elif OperatNum[0] == '/' and float(OperatNum[1]) == 0:
+        num -= int(OperatNum[1])
+    elif OperatNum[0] == '/' and int(OperatNum[1]) == 0:
         print("Деление на ноль запрещено. \nВведите знак деления '/' через пробел задайте другое число")
     elif OperatNum[0] == '/':
-        num /= float(OperatNum[1])
+        num /= int(OperatNum[1])
     elif OperatNum[0] == '**':
-        num **= float(OperatNum[1])
+        num **= int(OperatNum[1])
     elif OperatNum[0] == '=':
         flag = False
         print('Результат:', num)
