811fc4227ad003ad76ad76d8e9317fb4d35aa3c2
homrkova <79359305+homrkova@users.noreply.github.com>
2021-03-25 12:33:14 +0500

    Проверка числа на простоту

--- isPrime.py
+++ isPrime.py
@@ -1,18 +1,21 @@
-num = input()
+while True:
+    num = input()
+    if num.isdigit():
+        num = int(num)
+        break
+    else:
+        print('Была введена строка, а не число.\nВведите число.')
 count = 0
-if num.isdigit():
-    if int(num) == 0:
-        print(f'{num} - не является натуральным числом')
-    elif int(num) > 1:
-        for i in range(2, int(num) // 2 + 1):
-            if int(num) % i == 0:
+if int(num) == 0:
+    print(f'{num} - не является натуральным числом')
+elif int(num) > 1:
+    for i in range(2, int(num) // 2 + 1):
+        if int(num) % i == 0:
                 count = count + 1
-        if count == 0:
-            print(f'{num} - простое число')
-        else:
-            print(f'{num} - не простое число')
+    if count == 0:
+        print(f'{num} - простое число')
     else:
         print(f'{num} - не простое число')
 else:
-    print('Некорректный ввод')
+    print(f'{num} - не простое число')
 
